b0d40f964bc85a5127b4625bd6c9493a66ab4be2
Sahaduu <158268018+Sahaduu@users.noreply.github.com>
2024-05-11 20:26:45 +0600

    Update cvideo.js

--- scripts/cmds/cvideo.js
+++ scripts/cmds/cvideo.js
@@ -1,4 +1,4 @@
--cmd install cvid.js const axios = require("axios");
+const axios = require("axios");
 const fs = require('fs-extra');
 const path = require('path');
 const { getStreamFromURL, shortenURL, randomString } = global.utils;
