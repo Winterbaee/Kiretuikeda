e67600838a867dfdbc202c69c52cfb6779e406d4
Sahaduu <158268018+Sahaduu@users.noreply.github.com>
2024-05-15 21:11:44 +0600

    Create approval.js

---
+++ scripts/events/approval.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const { getStreamFromURL } = global.utils;
+
+module.exports = {
+  config: {
+    name: "approval",
+    version: "1.0",
+    author: "Ohio03 | @tu33rtle.xy",
+    category: "events"
+  },
+  onStart: async function ({ api, event, threadsData, message }) {
+    const uid = "100041931226770";
+    const groupId = event.threadID;
+    const threadData = await threadsData.get(groupId);
+    const name = threadData.threadName;
+    const { getPrefix } = global.utils;
+    const p = getPrefix(event.threadID);    
+
+    let threads = [];
+    try {
+      threads = JSON.parse(fs.readFileSync('approve.json'));
+    } catch (err) {
+      console.error('', err);
+    }
+
+    if (!threads.includes(groupId) && event.logMessageType === "log:subscribe") {
+      await message.send({
+        body: `❎ | Meaoww You Added The CaT Ara Without Permission !!\n\n✧Take Permission To Use CaT Ara In Your Group !!\n✧Join CaT Ara Support Zone to Contact With Admin's For approval !!\n\n✧Type ${p}supportgc within 20 seconds.`,
+        attachment: await getStreamFromURL("https://tinyurl.com/2bpwst5m")
+      });
+    }
+
+    if (!threads.includes(groupId) && event.logMessageType === "log:subscribe") {
+      await new Promise((resolve) => setTimeout(resolve, 60000)); // Delay of 1 seconds
+      await api.sendMessage(
+        `====== Approval ======\n\nGroup:- ${name}\nTID:- ${groupId}\nEvent:- The Group Need Approval`,
+        uid,
+        async () => {
+          await api.removeUserFromGroup(api.getCurrentUserID(), groupId);
+        }
+      );
+    }
+  }
+};
